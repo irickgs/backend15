7fdf60908d18db74d355666208a25f4baafee085
irickgs <135670382+irickgs@users.noreply.github.com>
2024-06-27 23:09:04 -0300

    otra prueba

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Grupo 15 - Comisión 24256");
+    res.send("Grupo 15 - Comisión 24256 muy bueno!");
 });
 
 const PORT = process.env.PORT || 3000;
